9dcd55c3b49aa14fb95e61c707f108becd1b6ef5
Mikhail <mitutee@gmail.com>
2018-03-19 14:55:20 +0000

    Add .c sources

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
+import Control.Concurrent (threadDelay)
+
 import Uhouse.Hardware.DHT11
 
 main :: IO ()
-main = someFunc
+main = do
+    r <- dhtReadRaw 11 4
+    print r
+    threadDelay 2000000
+    main
--- src/Uhouse/Hardware/DHT11.hs
+++ src/Uhouse/Hardware/DHT11.hs
@@ -8,7 +8,7 @@ import qualified Language.C.Inline as C
 
 C.include "<stdio.h>"
 C.include "myconv.h"
-C.include "mysens.h"
+C.include "pi_dht_read.h"
 
 dhtReadRaw :: CInt -> CInt -> IO CInt
 dhtReadRaw sensor pin = [C.block| int {
@@ -16,6 +16,12 @@ dhtReadRaw sensor pin = [C.block| int {
       int res = pi_dht_read($(int sensor), $(int pin), &tem, &hum);
       return pack(res, tem, hum);
   }|]
+
+type Sensor = Int
+type Pin = Int
+
+-- dhtRead :: Sesor -> Pin -> IO SensorReadResult
+-- dhtRead sens pin = dhtReadRaw sens pin >>=
 
 data SensorReadResult = Error ReadError | Result SensorData deriving (Show)
 
