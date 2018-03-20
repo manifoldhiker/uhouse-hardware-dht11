8203156995c37838431a6279b4f09c73c724b3d3
Mikhail <mitutee@gmail.com>
2018-03-20 08:19:07 +0000

    Polish and run

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,29 @@
 module Main where
 
 import Control.Concurrent (threadDelay)
+import Data.DateTime (getCurrentTime)
 
 import Uhouse.Hardware.DHT11
 
-main :: IO ()
+writePrLog :: String -> IO ()
+writePrLog = log' "station.test.log"
+
+logWeather :: String -> IO ()
+logWeather = log' "station.weather.log"
+
+log' fileName txt = do
+    currentTime <- show <$> getCurrentTime
+    let logMsg = currentTime++"<  --  >"++txt++"\n"
+    appendFile fileName logMsg
+    putStr logMsg
+
 main = do
-    r <- dhtRead 11 4
-    print r
-    threadDelay 2000000
-    main
+    writePrLog "Starting.."
+    go
+      where
+        go :: IO ()
+        go = do
+            r <- dhtRead 11 4
+            logWeather (show r)
+            threadDelay 2000000
+            go
